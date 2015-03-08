d7f4587097f16429a0529b6248aa9a05c0897db8
Mathias Renner <mail@mathias-renner.de>
2015-03-08 17:37:28 +0100

    added reference to hypriot blog

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
   public static void main (String[] args)
   {
     // Ausgabe Hello World!
-    System.out.println("Hello World!");
+    System.out.println("Hello World :-) Check out more fast-forward examples at blog.hypriot.com!");
   }
 }
