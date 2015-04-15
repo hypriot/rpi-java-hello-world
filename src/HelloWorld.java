f0c421dfdcd47eb0fd28e28abf4b78239c8014eb
Mathias Renner <mail@mathias-renner.de>
2015-04-15 11:54:37 +0200

    formatted app output nicely

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,11 @@ public class HelloWorld
   public static void main (String[] args)
   {
     // Ausgabe Hello World!
-    System.out.println("Hello World :-) Check out more fast-forward examples at blog.hypriot.com!");
+    System.out.println(
+	"+++++" + "\n" +
+	"Hey there!" + "\n" +
+	"I am a simple Hello World app :-)" + "\n" +
+	"Check out more fast-forward examples at blog.hypriot.com!" + "\n" +
+	"+++++");
   }
 }
